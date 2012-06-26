37f827b306d2b7c587cfb74dfcd7f101970a40f1
Matt Sanders <matt@modal.org>
2012-06-26 13:20:27 -0500

    Add simple CounterCache

---
+++ test/unit/counter_cache_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MetricsRailsTest < ActiveSupport::TestCase
+  
+  test 'basic operations' do
+    cc = MetricsRails::CounterCache.new
+    cc.increment :foo
+    assert_equal 1, cc[:foo]
+    
+    # accepts optional argument
+    cc.increment :foo, 5
+    assert_equal 6, cc[:foo]
+    
+    # strings or symbols work
+    cc.increment 'foo'
+    assert_equal 7, cc['foo']
+  end
+  
+end
