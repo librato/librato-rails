cb9a216213a5a3d8021dc03352072acbf08a73bb
Matt Sanders <matt@modal.org>
2012-06-26 14:57:15 -0500

    Add ability to flush metrics

--- lib/metrics/rails.rb
+++ lib/metrics/rails.rb
@@ -1,21 +1,27 @@
-#require 'activesupport/notifications'
+require 'active_support/notifications'
 
 require 'metrics/rails/version'
 
 module Metrics
   module Rails
+    extend SingleForwardable
+    
+    def_delegators :counters, :increment
 
     class << self
     
+      # access to client
       def client
       end
   
+      # access to internal counters object
       def counters
         @counter_cache ||= CounterCache.new
       end
-    
-      def increment(key, by=1)
-        counters.increment(key, by)
+      
+      # remove any accumulated but unsent metrics
+      def flush
+        counters.flush
       end
     
     end
@@ -31,6 +37,10 @@ module Metrics
     
       def [](key)
         @cache[key.to_s]
+      end
+      
+      def flush
+        @cache = {}
       end
     
       def increment(counter, by=1)
