09be7f00c1a50f1f9ee93f1435f92ebad07c1cc7
Matt Sanders <matt@modal.org>
2012-06-27 21:24:29 -0500

    Add ability to flush per request for local testing.

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  #after_filter :flush_metrics_rails
+  
+  # manually flush per request
+  def flush_metrics_rails
+    Metrics::Rails.flush
+  end
 end
