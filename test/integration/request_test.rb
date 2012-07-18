bc6e9fb4501ebfc928c1b1465403fe875a5b7333
Matt Sanders <matt@modal.org>
2012-07-18 09:38:26 -0500

    Clean up tests for customizable prefix.

--- test/integration/request_test.rb
+++ test/integration/request_test.rb
@@ -5,45 +5,49 @@ class RequestTest < ActiveSupport::IntegrationCase
   # Each request
   
   test 'increment total and status' do
+    prefix = Metrics::Rails.prefix
     visit root_path
     
-    assert_equal 1, counters['request.total']
-    assert_equal 1, counters['request.status.200']
-    assert_equal 1, counters['request.status.2xx']
+    assert_equal 1, counters["#{prefix}.request.total"]
+    assert_equal 1, counters["#{prefix}.request.status.200"]
+    assert_equal 1, counters["#{prefix}.request.status.2xx"]
     
     visit '/status/204'
     
-    assert_equal 2, counters['request.total']
-    assert_equal 1, counters['request.status.200']
-    assert_equal 1, counters['request.status.204']
-    assert_equal 2, counters['request.status.2xx']
+    assert_equal 2, counters["#{prefix}.request.total"]
+    assert_equal 1, counters["#{prefix}.request.status.200"]
+    assert_equal 1, counters["#{prefix}.request.status.204"]
+    assert_equal 2, counters["#{prefix}.request.status.2xx"]
   end
   
   test 'request times' do
+    prefix = Metrics::Rails.prefix
     visit root_path
     
     # common for all paths
-    assert_equal 1, aggregate['request.time'][:count], 'should record total time'
-    assert_equal 1, aggregate['request.time.db'][:count], 'should record db time'
-    assert_equal 1, aggregate['request.time.view'][:count], 'should record view time'
+    assert_equal 1, aggregate["#{prefix}.request.time"][:count], 'should record total time'
+    assert_equal 1, aggregate["#{prefix}.request.time.db"][:count], 'should record db time'
+    assert_equal 1, aggregate["#{prefix}.request.time.view"][:count], 'should record view time'
     
     # status specific
-    assert_equal 1, aggregate['request.status.200.time'][:count]
-    assert_equal 1, aggregate['request.status.2xx.time'][:count]
+    assert_equal 1, aggregate["#{prefix}.request.status.200.time"][:count]
+    assert_equal 1, aggregate["#{prefix}.request.status.2xx.time"][:count]
   end
   
   test 'track exceptions' do
+    prefix = Metrics::Rails.prefix
     begin
       visit exception_path #rescue nil
     rescue RuntimeError => e
       raise unless e.message == 'test exception!'
     end
-    assert_equal 1, counters['request.exceptions']
+    assert_equal 1, counters["#{prefix}.request.exceptions"]
   end
   
   test 'track slow requests' do
+    prefix = Metrics::Rails.prefix
     visit slow_path
-    assert_equal 1, counters['request.slow']
+    assert_equal 1, counters["#{prefix}.request.slow"]
   end
   
 end
