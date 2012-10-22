358ae74544551b4019706faab5879f6fd1fef7f9
Matt Sanders <matt@modal.org>
2012-10-22 09:44:27 -0500

    Add custom source support to counter cache.

--- lib/librato/rails/counter_cache.rb
+++ lib/librato/rails/counter_cache.rb
@@ -2,6 +2,8 @@ module Librato
   module Rails
     
     class CounterCache
+      DEFAULT_SOURCE = '%%'
+      
       extend Forwardable
     
       def_delegators :@cache, :empty?
@@ -11,30 +13,86 @@ module Librato
         @lock = Mutex.new
       end
     
+      # Retrieve the current value for a given metric. This is a short
+      # form for convenience which only retrieves metrics with no custom
+      # source specified. For more options see #fetch.
+      #
+      # @param [String|Symbol] key metric name
+      # @return [Integer|Float] current value
       def [](key)
-        @lock.synchronize { @cache[key.to_s] }
+        @lock.synchronize do 
+          @cache[key.to_s][DEFAULT_SOURCE] 
+        end
       end
       
+      # removes all tracked metrics. note this removes all measurement
+      # data AND metric names any continuously tracked metrics will not
+      # report until they get another measurement
       def delete_all
         @lock.synchronize { @cache.clear }
       end
       
+      
+      def fetch(key, options={})
+        source = options[:source].to_s || DEFAULT_SOURCE
+        @lock.synchronize do 
+          @cache[key.to_s][source]
+        end
+      end
+      
       def flush_to(queue)
         counts = nil
+        # work off of a duplicate data set so we block for
+        # as little time as possible
         @lock.synchronize do
           counts = @cache.dup
-          @cache.each_key { |key| @cache[key] = 0 }
+          reset_cache
         end
-        counts.each do |key, value| 
-          queue.add key => value
+        counts.each do |key, data|
+          data.each do |source, value|
+            if source == DEFAULT_SOURCE
+              queue.add key => value
+            else
+              queue.add key => {:value => value, :source => source}
+            end
+          end
         end
       end
     
-      def increment(counter, by=1)
+      # Increment a given metric
+      #
+      # @example Increment metric 'foo' by 1
+      #   increment :foo
+      #
+      # @example Increment metric 'bar' by 2
+      #   increment :bar, :by => 2
+      #
+      # @example Increment metric 'foo' by 1 with a custom source
+      #   increment :foo, :source => user.id
+      #
+      def increment(counter, options={})
+        source = DEFAULT_SOURCE
+        if options.is_a?(Fixnum) # suppport legacy style
+          by = options
+        else
+          by = options[:by] || 1
+          if options[:source]
+            source = options[:source].to_s
+          end
+        end
         counter = counter.to_s
         @lock.synchronize do
-          @cache[counter] ||= 0
-          @cache[counter] += by
+          @cache[counter] ||= {}
+          @cache[counter][source] ||= 0
+          @cache[counter][source] += by
+        end
+      end
+      
+    private
+    
+      def reset_cache
+        @cache.each_key do |key|
+          @cache[key].each_key { |source| @cache[key][source] = 0 }
         end
       end
     
--- test/unit/counter_cache_test.rb
+++ test/unit/counter_cache_test.rb
@@ -8,12 +8,31 @@ class LibratoRailsCounterCacheTest < MiniTest::Unit::TestCase
     assert_equal 1, cc[:foo]
     
     # accepts optional argument
-    cc.increment :foo, 5
+    cc.increment :foo, :by => 5
     assert_equal 6, cc[:foo]
     
     # strings or symbols work
     cc.increment 'foo'
     assert_equal 7, cc['foo']
+  end
+  
+  def test_custom_sources
+    cc = Librato::Rails::CounterCache.new
+    
+    cc.increment :foo, :source => 'bar'
+    assert_equal 1, cc.fetch(:foo, :source => 'bar')
+    
+    # symbols also work
+    cc.increment :foo, :source => :baz
+    assert_equal 1, cc.fetch(:foo, :source => :baz)
+    
+    # strings and symbols are interchangable
+    cc.increment :foo, :source => :bar
+    assert_equal 2, cc.fetch(:foo, :source => 'bar')
+     
+    # custom source and custom increment
+    cc.increment :foo, :source => 'boombah', :by => 10
+    assert_equal 10, cc.fetch(:foo, :source => 'boombah')
   end
   
 end
