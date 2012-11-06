991f0ee228ba72fafd2a036cbb300e04ddc294ee
Matt Sanders <matt@modal.org>
2012-11-06 09:53:28 -0600

    Fix exception if env missing in config; always respect log_level ENV

--- lib/librato/rails.rb
+++ lib/librato/rails.rb
@@ -54,13 +54,17 @@ module Librato
 
       # detect / update configuration
       def check_config
+        self.log_level = ENV['LIBRATO_METRICS_LOG_LEVEL'] if ENV['LIBRATO_METRICS_LOG_LEVEL']
         if self.config_file && File.exists?(self.config_file)
-          log :debug, "configuration file present, ignoring ENV variables"
-          env_specific = YAML.load(ERB.new(File.read(config_file)).result)[::Rails.env]
-          settable = CONFIG_SETTABLE & env_specific.keys
-          settable.each { |key| self.send("#{key}=", env_specific[key]) }
+          log :debug, "configuring with librato.yml; ignoring environment variables.."
+          if env_specific = YAML.load(ERB.new(File.read(config_file)).result)[::Rails.env]
+            settable = CONFIG_SETTABLE & env_specific.keys
+            settable.each { |key| self.send("#{key}=", env_specific[key]) }
+          else
+            log :debug, "current environment not in config file, halting"
+          end
         else
-          log :debug, "no configuration file present, using ENV variables"
+          log :debug, "using environment variables for configuration.."
           %w{user token source log_level}.each do |settable|
             env_var = "LIBRATO_METRICS_#{settable.upcase}"
             send("#{settable}=", ENV[env_var]) if ENV[env_var]
