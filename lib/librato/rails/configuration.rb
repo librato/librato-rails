5dc1cb5dfbc3a2b3513173ef8b3ff7817ab83fef
Matt Sanders <matt@modal.org>
2013-08-19 16:28:28 -0700

    Refactor configuration to subclass lib-rack Configuration class

--- lib/librato/rails/configuration.rb
+++ lib/librato/rails/configuration.rb
@@ -1,50 +1,47 @@
 module Librato
   module Rails
-    module Configuration
+    # Adds yaml-based config and extra properties to the configuration
+    # class provided by librato-rack
+    #
+    # https://github.com/librato/librato-rack/blob/master/lib/librato/rack/configuration.rb
+    #
+    class Configuration < Rack::Configuration
       CONFIG_SETTABLE = %w{user token flush_interval log_level prefix source source_pids}
 
-      mattr_accessor :config_file
-      self.config_file = 'config/librato.yml'
+      attr_accessor :config_by, :config_file
 
-      # set custom api endpoint
-      def api_endpoint=(endpoint)
-        @api_endpoint = endpoint
+      # options:
+      # * :config_file - alternate config file location
+      #
+      def initialize(options={})
+        self.config_file = options[:config_file] || 'config/librato.yml'
+        super()
       end
 
-      # detect / update configuration
-      def check_config
-        self.log_level = ENV['LIBRATO_LOG_LEVEL'] if ENV['LIBRATO_LOG_LEVEL']
+      def configure_with_config_file
+        # log :debug, "configuring with librato.yml; ignoring environment variables.."
+        self.config_by = :config_file
+        env_specific = YAML.load(ERB.new(File.read(config_file)).result)[::Rails.env]
+        if env_specific
+          settable = CONFIG_SETTABLE & env_specific.keys
+          settable.each { |key| self.send("#{key}=", env_specific[key]) }
+        # else
+        #   log :debug, "halting: current environment (#{::Rails.env}) not in config file."
+        end
+      end
+
+      # detect and load configuration from config file or env vars
+      def load_configuration
         if self.config_file && File.exists?(self.config_file)
           configure_with_config_file
         else
-          configure_with_environment
+          self.config_by = :environment
+          super
         end
-      end
 
-      private
-
-      def configure_with_config_file
-        log :debug, "configuring with librato.yml; ignoring environment variables.."
-        if env_specific = YAML.load(ERB.new(File.read(config_file)).result)[::Rails.env]
-          settable = CONFIG_SETTABLE & env_specific.keys
-          settable.each { |key| self.send("#{key}=", env_specific[key]) }
-        else
-          log :debug, "halting: current environment (#{::Rails.env}) not in config file."
-        end
-      end
-
-      def configure_with_environment
-        log :debug, "using environment variables for configuration.."
-        %w{user token source log_level prefix}.each do |settable|
-          legacy_env_var = "LIBRATO_METRICS_#{settable.upcase}"
-          if ENV[legacy_env_var]
-            log :warn, "#{legacy_env_var} is deprecated, use LIBRATO_#{settable.upcase} instead"
-            send("#{settable}=", ENV[legacy_env_var])
-          end
-          # if both are present, new-style dominates
-          env_var = "LIBRATO_#{settable.upcase}"
-          send("#{settable}=", ENV[env_var]) if ENV[env_var]
-        end
+        # respect autorun and log_level env vars regardless of config method
+        self.autorun = detect_autorun
+        self.log_level = ENV['LIBRATO_LOG_LEVEL'] if ENV['LIBRATO_LOG_LEVEL']
       end
 
     end
