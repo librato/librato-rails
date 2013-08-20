4ba779d014e8a6f724706e6b90b38c2f2ff30371
Matt Sanders <matt@modal.org>
2013-08-20 10:23:15 -0700

    Clean up old whitespace

--- test/integration/mail_test.rb
+++ test/integration/mail_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class MailTest < ActiveSupport::IntegrationCase
-  
+
   # Query tests - the numbers specified assume running against SQLite
-  
+
   test 'mail sent' do
     user = User.create!(:email => 'foo@foo.com', :password => 'wow')
     UserMailer.welcome_email(user).deliver
     assert_equal 1, counters["rails.mail.sent"]
   end
-  
+
 end
