4af3feb189a8a88a26a9de49c461b66e6751254f
Matt Sanders <matt@modal.org>
2013-08-20 10:21:11 -0700

    Refactor subscribers to use Librato root calls

--- lib/librato/rails/subscribers.rb
+++ lib/librato/rails/subscribers.rb
@@ -1,77 +1,77 @@
+require 'active_support/notifications'
+
 module Librato
   module Rails
+    module Subscribers
 
-    # controllers
+      # Controllers
 
-    ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args|
+      ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |*args|
 
-      event = ActiveSupport::Notifications::Event.new(*args)
-      controller = event.payload[:controller]
-      action = event.payload[:action]
+        event = ActiveSupport::Notifications::Event.new(*args)
+        controller = event.payload[:controller]
+        action = event.payload[:action]
 
-      format = event.payload[:format] || "all"
-      format = "all" if format == "*/*"
-      status = event.payload[:status]
-      exception = event.payload[:exception]
-      # page_key = "request.#{controller}.#{action}_#{format}."
+        format = event.payload[:format] || "all"
+        format = "all" if format == "*/*"
+        status = event.payload[:status]
+        exception = event.payload[:exception]
+        # page_key = "request.#{controller}.#{action}_#{format}."
 
-      group "rails.request" do |r|
+        Librato.group "rails.request" do |r|
 
-        r.increment 'total'
-        r.timing    'time', event.duration
+          r.increment 'total'
+          r.timing    'time', event.duration
 
-        if exception
-          r.increment 'exceptions'
-        else
-          r.timing 'time.db', event.payload[:db_runtime] || 0
-          r.timing 'time.view', event.payload[:view_runtime] || 0
-        end
-
-        unless status.blank?
-          r.group 'status' do |s|
-            s.increment status
-            s.increment "#{status.to_s[0]}xx"
-            s.timing "#{status}.time", event.duration
-            s.timing "#{status.to_s[0]}xx.time", event.duration
+          if exception
+            r.increment 'exceptions'
+          else
+            r.timing 'time.db', event.payload[:db_runtime] || 0
+            r.timing 'time.view', event.payload[:view_runtime] || 0
           end
+
+          unless status.blank?
+            r.group 'status' do |s|
+              s.increment status
+              s.increment "#{status.to_s[0]}xx"
+              s.timing "#{status}.time", event.duration
+              s.timing "#{status.to_s[0]}xx.time", event.duration
+            end
+          end
+
+          r.increment 'slow' if event.duration > 200.0
+        end # end group
+
+      end # end subscribe
+
+      # SQL
+
+      ActiveSupport::Notifications.subscribe 'sql.active_record' do |*args|
+        payload = args.last
+
+        Librato.group "rails.sql" do |s|
+          # puts (event.payload[:name] || 'nil') + ":" + event.payload[:sql] + "\n"
+          s.increment 'queries'
+
+          sql = payload[:sql].strip
+          s.increment 'selects' if sql.starts_with?('SELECT')
+          s.increment 'inserts' if sql.starts_with?('INSERT')
+          s.increment 'updates' if sql.starts_with?('UPDATE')
+          s.increment 'deletes' if sql.starts_with?('DELETE')
         end
-
-        r.increment 'slow' if event.duration > 200.0
-      end # end group
-
-    end # end subscribe
-
-    # SQL
-
-    ActiveSupport::Notifications.subscribe 'sql.active_record' do |*args|
-      payload = args.last
-
-      group "rails.sql" do |s|
-        # puts (event.payload[:name] || 'nil') + ":" + event.payload[:sql] + "\n"
-        s.increment 'queries'
-
-        sql = payload[:sql].strip
-        s.increment 'selects' if sql.starts_with?('SELECT')
-        s.increment 'inserts' if sql.starts_with?('INSERT')
-        s.increment 'updates' if sql.starts_with?('UPDATE')
-        s.increment 'deletes' if sql.starts_with?('DELETE')
       end
-    end
 
-    # ActionMailer
+      # ActionMailer
 
-    ActiveSupport::Notifications.subscribe 'deliver.action_mailer' do |*args|
-      # payload[:mailer] => 'UserMailer'
-      group "rails.mail" do |m|
-        m.increment 'sent'
+      ActiveSupport::Notifications.subscribe 'deliver.action_mailer' do |*args|
+        # payload[:mailer] => 'UserMailer'
+        Librato.increment "rails.mail.sent"
       end
-    end
 
-    ActiveSupport::Notifications.subscribe 'receive.action_mailer' do |*args|
-      group "rails.mail" do |m|
-        m.increment 'received'
+      ActiveSupport::Notifications.subscribe 'receive.action_mailer' do |*args|
+        Librato.increment "rails.mail.received"
       end
-    end
 
+    end
   end
 end
