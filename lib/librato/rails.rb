bbb67786b43fc9a1ab8e14b164759578fc67ca56
Matt Sanders <matt@modal.org>
2013-08-19 17:07:19 -0700

    Move setup into railtie

--- lib/librato/rails.rb
+++ lib/librato/rails.rb
@@ -6,30 +6,6 @@ require 'librato/rails/configuration'
 require 'librato/rails/tracker'
 require 'librato/rails/version'
 
-module Librato
-  module Rails
-    class << self
-
-      # run once during Rails startup sequence
-      def setup(app)
-        check_config
-        trace_settings if should_log?(:debug)
-        return unless should_start?
-        if app_server == :other
-          log :info, "starting up..."
-        else
-          log :info, "starting up with #{app_server}..."
-        end
-        @pid = $$
-        app.middleware.use Librato::Rack::Middleware
-        start_worker unless forking_server?
-      end
-
-    end # end class << self
-
-  end
-end
-
 # must load after all module setup
 require 'librato/rails/railtie' if defined?(Rails)
 require 'librato/rails/subscribers'
