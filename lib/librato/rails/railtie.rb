f94e762c47d59c82d7d2c78f6ae261ad81f64fe2
Matt Sanders <matt@modal.org>
2013-08-19 17:35:48 -0700

    Use librato-rack with custom config in railtie

--- lib/librato/rails/railtie.rb
+++ lib/librato/rails/railtie.rb
@@ -3,14 +3,21 @@ module Librato
     class Railtie < ::Rails::Railtie
 
       # make configuration proxy for config inside Rails
-      config.librato_rails = Librato::Rails
+      config.librato_rails = Configuration.new
+      tracker = Tracker.new(config.librato_rails)
+      config.librato_rails.tracker = tracker
 
-      initializer 'librato_rails.setup' do |app|
-        # don't start in test mode or in the console
-        unless ::Rails.env.test? || defined?(::Rails::Console)
-          Librato::Rails.setup(app)
+      if !::Rails.env.test? && tracker.should_start?
+        unless defined?(::Rails::Console) && ENV['LIBRATO_AUTORUN'] != '1'
+
+          initializer 'librato_rails.setup' do |app|
+            tracker.log :info, "starting up..."
+            app.middleware.use Librato::Rack, :config => config.librato_rails
+          end
+
         end
       end
+
     end
   end
 end
