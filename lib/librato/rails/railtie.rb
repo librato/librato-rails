c1390e11b2b0cd079c2048ed78dd35f9f44525a5
Matt Sanders <matt@modal.org>
2013-08-20 13:09:29 -0700

    Ensure logging gets hooked up to the rails logger

--- lib/librato/rails/railtie.rb
+++ lib/librato/rails/railtie.rb
@@ -2,8 +2,13 @@ module Librato
   module Rails
     class Railtie < ::Rails::Railtie
 
+      # don't have any custom http vars anymore, check if hostname is UUID
+      on_heroku = Socket.gethostname =~ /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i
+
       # make configuration proxy for config inside Rails
       config.librato_rails = Configuration.new
+
+      # set up tracker
       tracker = Tracker.new(config.librato_rails)
       config.librato_rails.tracker = tracker
       Librato.register_tracker(tracker)
@@ -12,6 +17,12 @@ module Librato
         unless defined?(::Rails::Console) && ENV['LIBRATO_AUTORUN'] != '1'
 
           initializer 'librato_rails.setup' do |app|
+            # set up logging; heroku needs logs to STDOUT which librato-rack
+            # will handle by default
+            unless on_heroku
+              config.librato_rails.log_target = ::Rails.logger
+            end
+
             tracker.log :info, "starting up..."
             app.middleware.use Librato::Rack, :config => config.librato_rails
           end
