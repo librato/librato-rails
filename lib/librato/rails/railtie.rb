7996936a81f5908472cc4d8e3c83d9a47adede52
Zack Siri <zack@codemy.net>
2016-03-08 15:53:13 +0700

    use before_configuration block

--- lib/librato/rails/railtie.rb
+++ lib/librato/rails/railtie.rb
@@ -5,14 +5,17 @@ module Librato
       # don't have any custom http vars anymore, check if hostname is UUID
       on_heroku = Socket.gethostname =~ /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/i
 
-      initializer 'librato_rails.setup' do |app|
+      config.before_configuration do 
         # make configuration proxy for config inside Rails
-        app.config.librato_rails = Configuration.new
+        config.librato_rails = Configuration.new
 
         # set up tracker
-        tracker = Tracker.new(app.config.librato_rails)
-        app.config.librato_rails.tracker = tracker
+        tracker = Tracker.new(config.librato_rails)
+        config.librato_rails.tracker = tracker
         Librato.register_tracker(tracker)
+      end
+
+      initializer 'librato_rails.setup' do |app|
 
         ActiveSupport.on_load :action_controller do
           extend Librato::Rails::Helpers::Controller
@@ -27,12 +30,12 @@ module Librato
             logger = ::Rails.logger
           end
           config.librato_rails.log_target = logger
-          tracker.log(:debug) { "config: #{config.librato_rails.dump}" }
+          config.librato_rails.tracker.log(:debug) { "config: #{config.librato_rails.dump}" }
 
-          if tracker.should_start?
-            tracker.log :info, "starting up (pid #{$$}, using #{config.librato_rails.config_by})..."
+          if config.librato_rails.tracker.should_start?
+            config.librato_rails.tracker.log :info, "starting up (pid #{$$}, using #{config.librato_rails.config_by})..."
             app.middleware.insert(0, Librato::Rack, :config => config.librato_rails)
-            tracker.check_worker if config.librato_rails.autorun
+            config.librato_rails.tracker.check_worker if config.librato_rails.autorun
           end
         end
 
