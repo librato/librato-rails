3d8a4c8ecca5229b4019aafc04fad93d85e1cd14
Matt Sanders <matt@modal.org>
2016-03-30 15:33:48 -0700

    Disable view render instrumentation

--- lib/librato/rails/subscribers/render.rb
+++ lib/librato/rails/subscribers/render.rb
@@ -1,28 +1,28 @@
-module Librato
-  module Rails
-    module Subscribers
-
-      # Render operations
-
-      %w{partial template}.each do |metric|
-
-        ActiveSupport::Notifications.subscribe "render_#{metric}.action_view" do |*args|
-          event = ActiveSupport::Notifications::Event.new(*args)
-          path = event.payload[:identifier].split('/views/', 2)
-
-          if path[1]
-            source = path[1].gsub('/', ':')
-            # trim leading underscore for partial sources
-            source.gsub!(':_', ':') if metric == 'partial'
-            collector.group "rails.view" do |c|
-              c.increment "render_#{metric}", source: source, sporadic: true
-              c.timing "render_#{metric}.time", event.duration, source: source, sporadic: true
-            end
-          end
-        end
-
-      end
-
-    end
-  end
-end
+# module Librato
+#   module Rails
+#     module Subscribers
+#
+#       # Render operations
+#
+#       %w{partial template}.each do |metric|
+#
+#         ActiveSupport::Notifications.subscribe "render_#{metric}.action_view" do |*args|
+#           event = ActiveSupport::Notifications::Event.new(*args)
+#           path = event.payload[:identifier].split('/views/', 2)
+#
+#           if path[1]
+#             source = path[1].gsub('/', ':')
+#             # trim leading underscore for partial sources
+#             source.gsub!(':_', ':') if metric == 'partial'
+#             collector.group "rails.view" do |c|
+#               c.increment "render_#{metric}", source: source, sporadic: true
+#               c.timing "render_#{metric}.time", event.duration, source: source, sporadic: true
+#             end
+#           end
+#         end
+#
+#       end
+#
+#     end
+#   end
+# end
