78eb3251fc461f0a8f46910ae8ba23e8422034e5
Chance Feick <chance@chance.io>
2017-01-24 01:37:58 -0800

    add inherit_tags to mail subscriber

--- lib/librato/rails/subscribers/mail.rb
+++ lib/librato/rails/subscribers/mail.rb
@@ -7,13 +7,13 @@ module Librato
       ActiveSupport::Notifications.subscribe "deliver.action_mailer" do |*args|
         event = ActiveSupport::Notifications::Event.new(*args)
         tags = { mailer: event.payload[:mailer] }
-        collector.increment "rails.mail.sent", tags: tags
+        collector.increment "rails.mail.sent", tags: tags, inherit_tags: true
       end # end subscribe
 
       ActiveSupport::Notifications.subscribe "receive.action_mailer" do |*args|
         event = ActiveSupport::Notifications::Event.new(*args)
         tags = { mailer: event.payload[:mailer] }
-        collector.increment "rails.mail.received", tags: tags
+        collector.increment "rails.mail.received", tags: tags, inherit_tags: true
       end # end subscribe
 
     end
